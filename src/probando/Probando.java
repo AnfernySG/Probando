e3e03ede742bf7568fca552fe8ad91b5fed66c7a
AnfernySG <144975884+AnfernySG@users.noreply.github.com>
2023-10-15 15:50:36 -1200

    Update Probando.java

--- src/probando/Probando.java
+++ src/probando/Probando.java
@@ -14,7 +14,10 @@ public class Probando {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Pueba");      
+        System.out.println("Pueba"); 
+        int entero = 5;
+        string nombre 
+            system.out.print
     }
     
 }
